7a4174a6385e5dca801c3219a7cb9ccdbb1ef737
pranavbantval <league@iMac.attlocal.net>
2017-12-29 18:27:10 -0800

    Change Calculator, Test Scores(IP)

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -1,7 +1,6 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
 import javax.swing.JOptionPane;
 
 /*
@@ -14,17 +13,19 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String a = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+		int x = Integer.parseInt(a);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String b = JOptionPane.showInputDialog("How many dimes do you have?");
+		int y = Integer.parseInt(b);
 		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has and save it in a double variable 
-
+		String c = JOptionPane.showInputDialog("How many quarters do you have?");
+		int z = Integer.parseInt(c);
+		// Calculate how much money the user has and save it in a double variable
+		double money = (0.25 * z) + (0.1 * y) + (0.05 * x);
 		// Tell the user how much money they have
+		JOptionPane.showMessageDialog(null, "You have $" + money);
 
 	}
 }
-
---
+++ src/TestScores.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class TestScores {
+public static void main(String[] args) {
+	String n = JOptionPane.showInputDialog("What test score did you get?");
+	int x = Integer.parseInt(n);
+	if(x>=90) {
+		JOptionPane.showMessageDialog(null, "Great job!");
+	}
+	else if (x>=80&&x<90) {
+		JOptionPane.showMessageDialog(null, "Good job!");
+	}
+	else if (x>=70&&x<80) {
+		JOptionPane.showMessageDialog(null, "You passed nice job.");
+	}
+}
+}
