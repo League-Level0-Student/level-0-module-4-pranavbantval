684fef3da2206ffa9886c6c16e50fc0a513b85ba
League <league@WTS8.attlocal.net>
2018-01-12 18:31:31 -0800

    Pi Aloud, Own Adventure, Happy Pet, String Methods

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -6,28 +6,69 @@
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	// 2. Create a happinessLevel variable to store the pet's happiness number.
+	static // 2. Create a happinessLevel variable to store the pet's happiness number.
 	//    Initialize to zero.
-
+int happyLevel = 0;
+	static String  pet;
 	public static void main(String[] args) {
+		Object[] c = {"cuddle it"," feed it","give it water","take a walk with it", "groom it"  };
 		// 1. Ask the user what kind of pet they want to buy, and store in variable
-
+  pet = JOptionPane.showInputDialog("What kind of pet do you want?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+	for (int i = 0; i < 5; i++) {
 		
+	
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
+			//    (Egg: cuddle, food, water, take a walk, groom, clean up pop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", " ",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null,c,c[0]);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+if(task==0) {
+	pet();
+}
+else if(task==1) {
+	food();
+}
+else if(task==2) {
+	water();
+}
+else if(task==3) {
+	walk();
+}
+else if(task==4) {
+	groom();
+}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+if(happyLevel==5) {
+	JOptionPane.showMessageDialog(null, "You really love your pet!");
+	break;
+}}
 	}
 
 	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
+	//    Each method should create a pop-up with the pet's response (egg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	 static void pet() {
+		JOptionPane.showMessageDialog(null, "Your "+ pet +" is happy!");
+		happyLevel++;
+	}
+	static void food() {
+		JOptionPane.showMessageDialog(null, "Your "+pet+" is no longer starving!");
+		happyLevel++;
+	}
+	static void water() {
+		JOptionPane.showMessageDialog(null, "Your "+pet+" is no longer dehydrated!");
+		happyLevel++;
+	}
+	static void walk() {
+		JOptionPane.showMessageDialog(null, "Your "+pet+" has got its daily exercise!");
+		happyLevel++;
+	}
+	static void groom() {
+		JOptionPane.showMessageDialog(null, "Your "+pet+" is looking fancy!");
+		happyLevel++;
+	}
 }
--- src/OwnAdventure.java
+++ src/OwnAdventure.java
@@ -1,16 +1,35 @@
 import javax.swing.JOptionPane;
 
 public class OwnAdventure {
-public static void main(String[] args) {
-	Object[] b = {"the park", "the pool"};
-	Object[] c = {"cook", "play video games"};
-	int a =	JOptionPane.showConfirmDialog(null, "You just woke up. Do you want to go for a walk?", "", JOptionPane.YES_NO_OPTION);
-if (a==0) {
-	int bInt = JOptionPane.showOptionDialog(null, "Where do you want to go?", " ", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, b, b[0]);
-}
-else if(a==1) {
-	JOptionPane.showOptionDialog(null, "What indoor activity do you want to do?", " ", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, c, c[0]);	
-}
+	public static void main(String[] args) {
+		Object[] b = { "the park", "the beach" };
+		Object[] c = { "cook", "play video games" };
+		int bint = 0;
+		int dint = 0;
+		int a = JOptionPane.showConfirmDialog(null,
+				"You just woke up from a late night nap. Do you want to go for a walk?", "", JOptionPane.YES_NO_OPTION);
+		if (a == 0) {
+			bint = JOptionPane.showOptionDialog(null, "Where do you want to go?", " ", JOptionPane.DEFAULT_OPTION,
+					JOptionPane.QUESTION_MESSAGE, null, b, b[0]);dint = 2;
+		} else if (a == 1) {
+			dint = JOptionPane.showOptionDialog(null, "What indoor activity do you want to do?", " ",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, c, c[0]);bint = 2;
+		}
 
-}
+		if (bint == 0) {
+			JOptionPane.showMessageDialog(null,
+					"You had a great time at the park, but now you are exhausted. You decided to go to sleep.");
+		} else if (bint == 1) {
+			JOptionPane.showMessageDialog(null,
+					"You had a great time at the beach, but now you are exhausted. You decided to go to sleep");
+		}
+		if (dint == 0) {
+JOptionPane.showMessageDialog(null,
+		"You made a great meal for dinner, but you are exhausted. You decided to go to sleep");
+		}
+		else if(dint==1) {
+			JOptionPane.showMessageDialog(null,
+					"You had a great time playing video games, but you are exhausted. You decided to go to sleep");
+		}
+	}
 }
